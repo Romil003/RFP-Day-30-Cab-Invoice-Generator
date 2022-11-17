7cc3ad4220b067f1ceb5d72239792b5e7f39734f
Romil003 <romilghadge33@gmail.com>
2022-11-17 18:32:53 +0530

    Added Cab Invoice Generator UC2

---
+++ main/java/com/bridgelabz/Ride.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Ride {
+
+    double distance ;
+
+    double time ;
+
+    public Ride(double distance, double time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
--- test/java/com/bridgelabz/CabInvoiceTest.java
+++ test/java/com/bridgelabz/CabInvoiceTest.java
@@ -12,4 +12,19 @@ public class CabInvoiceTest {
         double totalFare = cabInvoice.calculateTotalFare(10.0,4.0);
         Assertions.assertEquals(104.0,totalFare);
     }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinimumFare(){
+        CabInvoice cabInvoice = new CabInvoice();
+        double totalFare = cabInvoice.calculateTotalFare(0.1,1.0);
+        Assertions.assertEquals(5.0,totalFare);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnAggregateTotalFare(){
+        CabInvoice cabInvoice = new CabInvoice();
+        Ride[] rides = {new Ride(10.0,4.0),new Ride(5.0,5.0),new Ride(0.1,1.0)};
+        double aggregateTotalFare = cabInvoice.calculateTotalFare(rides);
+        Assertions.assertEquals(164.0,aggregateTotalFare);
+    }
 }
